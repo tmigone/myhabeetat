a5dd29ec4bec7b00b56fb69be45469c9be2226ac
Tomás Migone <tomasmigone@gmail.com>
2019-06-20 23:13:54 -0300

    Removed config module. Replaced with plain config.js file

--- index.js
+++ index.js
@@ -1,8 +1,3 @@
-// Make sure we load from module config folder
-const path = require('path')
-process.env.NODE_CONFIG_DIR = path.join(__dirname, './config')
-process.env.SUPPRESS_NO_CONFIG_WARNING = 'y'
-
 // Export module
 const MyHabeetat = require('./lib/MyHabeetat.js')
 module.exports = MyHabeetat
